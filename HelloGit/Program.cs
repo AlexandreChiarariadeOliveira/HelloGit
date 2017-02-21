bc99b886be5e79a3cadedf2fe4686eb6861ddcca
univem <univem@univem.edu.br>
2017-02-20 22:45:54 -0300

    aula

--- HelloGit/Program.cs
+++ HelloGit/Program.cs
@@ -10,7 +10,19 @@ namespace HelloGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Git!");
+            string texto = "Rua abc, 35, Marilia, Sao Paulo";
+            string[] teste = texto.Split(',');
+
+            for (int i = 0; i < teste.Length; i++)
+            {
+                Console.WriteLine(teste[i]);
+            }
+
+            foreach (var c in teste)
+            {
+                Console.WriteLine(c);
+            }
+            
             Console.ReadKey();
         }
     }
